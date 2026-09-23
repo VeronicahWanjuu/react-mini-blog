86acef12b27930e33a33e08637158594ac8d9820
VeronicahWanjuu <v.wanjuu@alustudent.com>
2026-09-23 16:36:48 +0200

    add withLogger HOC that logs component mount and unmount

---
+++ src/components/withLogger.tsx
@@ -0,0 +1,21 @@
+﻿import React, { useEffect } from 'react';
+
+function withLogger<T extends object>(
+  WrappedComponent: React.ComponentType<T>,
+  componentName: string
+) {
+  const WithLogger: React.FC<T> = (props) => {
+    useEffect(() => {
+      console.log(`[withLogger] ${componentName} mounted`);
+      return () => {
+        console.log(`[withLogger] ${componentName} unmounted`);
+      };
+    }, []);
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return WithLogger;
+}
+
+export default withLogger;
